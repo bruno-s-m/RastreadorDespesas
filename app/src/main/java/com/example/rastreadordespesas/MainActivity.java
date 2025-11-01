888c2dca0d57f610694e3e9e43ea23dd3823328b
Bruno Sales Maia <bsalesmaia@gmail.com>
2025-11-01 10:00:03 -0300

    adição do lancamento das despesas

--- app/src/main/java/com/example/rastreadordespesas/MainActivity.java
+++ app/src/main/java/com/example/rastreadordespesas/MainActivity.java
@@ -6,6 +6,8 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -15,19 +17,32 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    //adicionar categorias
     private EditText txtCategoriaNome;
     private EditText txtCategoriaLimite;
     private Button btnSalvarCategoria;
-
+    //banco de dados
     private AppDatabase db;
-
+    //lista categoria
     private ListView listviewCategorias;
     private ArrayAdapter<String> categoryAdapter;
     private ArrayList<String> categoryList;
+    //despesas
+    private EditText txtValorDespesa;
+    private EditText txtDescricaoDespesa;
+    private Spinner spinCategorias;
+    private TextView txtDataDespesa;
+    private Button btnSalvarDespesa;
+    //lista despesa
+    private ArrayAdapter<String> spinAdapter;
+    private ArrayList<String> spinCategoryNames;
+    private List<CategoryEntity> loadedCategories;
+    //calendario
+    private Calendar dataSelecionada;
 
 
     @Override
@@ -53,7 +68,48 @@ public class MainActivity extends AppCompatActivity {
         categoryAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, categoryList);
         listviewCategorias.setAdapter(categoryAdapter);
 
+        spinCategorias = findViewById(R.id.spinCategorias);
+        spinCategoryNames = new ArrayList<>();
+        loadedCategories = new ArrayList<>();
+
+        spinAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, spinCategoryNames);
+        spinAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinCategorias.setAdapter(spinAdapter);
+
+
         carregarCategoriasDoBanco();
+
+        txtValorDespesa = findViewById(R.id.txtValorDespesa);
+        txtDescricaoDespesa = findViewById(R.id.txtDescricaoDespesa);
+        spinCategorias = findViewById(R.id.spinCategorias);
+        txtDataDespesa = findViewById(R.id.txtDataDespesa);
+        btnSalvarDespesa = findViewById(R.id.btnSalvarDespesa);
+
+        dataSelecionada = Calendar.getInstance();
+
+        atualizarDataNoTextView();
+
+        txtDataDespesa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int ano = dataSelecionada.get(Calendar.YEAR);
+                int mes = dataSelecionada.get(Calendar.MONTH);
+                int dia = dataSelecionada.get(Calendar.DAY_OF_MONTH);
+
+
+                android.app.DatePickerDialog datePickerDialog = new android.app.DatePickerDialog(MainActivity.this, new android.app.DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(android.widget.DatePicker view, int year, int month, int dayOfMonth) {
+                        dataSelecionada.set(year, month, dayOfMonth);
+                        atualizarDataNoTextView();
+                    }
+                }, ano, mes, dia);
+                datePickerDialog.show();
+            }
+
+
+        });
+
 
         btnSalvarCategoria.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,6 +117,18 @@ public class MainActivity extends AppCompatActivity {
                 salvarNovaCategoria();
             }
         });
+
+        btnSalvarDespesa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                salvarNovaDespesa();
+            }
+        });
+    }
+
+    private void atualizarDataNoTextView() {
+        java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("dd/MM/yyyy");
+        txtDataDespesa.setText(sdf.format(dataSelecionada.getTime()));
     }
 
     private void salvarNovaCategoria() {
@@ -109,21 +177,83 @@ public class MainActivity extends AppCompatActivity {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                List<CategoryEntity> categoriasDoBanco = db.categoryDao().getAllCategories();
+                loadedCategories = db.categoryDao().getAllCategories();
                 categoryList.clear();
+                spinCategoryNames.clear();
 
-                for (CategoryEntity categoria : categoriasDoBanco) {
+                spinCategoryNames.add("Selecione uma categoria");
+
+                for (CategoryEntity categoria : loadedCategories) {
                     String limiteFormatado = String.format("%.2f", categoria.getLimiteMensal());
                     categoryList.add(categoria.getName() + "(Limite: R$ " + limiteFormatado + " )");
+
+                    spinCategoryNames.add(categoria.getName());
                 }
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
                         categoryAdapter.notifyDataSetChanged();
+                        spinAdapter.notifyDataSetChanged();
                     }
                 });
             }
         }).start();
 
     }
+
+    private void salvarNovaDespesa() {
+        String valorStr = txtValorDespesa.getText().toString();
+        String descricao = txtDescricaoDespesa.getText().toString();
+
+        int spinPosition = spinCategorias.getSelectedItemPosition();
+
+        final java.util.Date dataParaSalvar = dataSelecionada.getTime();
+
+        if (valorStr.isEmpty()) {
+            Toast.makeText(this, "Por favor, insira um valor", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (spinPosition == 0) {
+            Toast.makeText(this, "Por favor, selecione uma categoria", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        double valor;
+        try {
+            valor = Double.parseDouble(valorStr);
+        } catch (NumberFormatException e) {
+            Toast.makeText(this, "Valor inválido. ", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        CategoryEntity categoriaSelecionada = loadedCategories.get(spinPosition - 1);
+        int categoriaId = categoriaSelecionada.getId();
+
+        final ExpenseEntity novaDespesa = new ExpenseEntity();
+        novaDespesa.setValor(valor);
+        novaDespesa.setDescricao(descricao);
+        novaDespesa.setData(dataParaSalvar);
+        novaDespesa.setCategoriaId(categoriaId);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                db.expenseDao().insertExpense(novaDespesa);
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "Despesa salva !", Toast.LENGTH_SHORT).show();
+                        txtValorDespesa.setText("");
+                        txtDescricaoDespesa.setText("");
+                        spinCategorias.setSelection(0);
+                    }
+                });
+            }
+        }).start();
+
+
+    }
+
 }
